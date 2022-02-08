46ef29ace0f0e932f58c89dffbf1f5934d616391
Tiago <tiagosantiniathayde@outlook.com>
2022-02-08 14:35:57 -0300

    Corrigindo validação do Exercicio02

--- ExercicioDois.ConsoleApp/Program.cs
+++ ExercicioDois.ConsoleApp/Program.cs
@@ -10,25 +10,39 @@ namespace ExercicioDois.ConsoleApp
         {
             int valorA, valorB, valorC;
 
-            Console.Write("Digite o valor A: ");
-            valorA = Convert.ToInt32(Console.ReadLine());
-            
-            Console.Write("Digite o valor B: ");
-            valorB = Convert.ToInt32(Console.ReadLine());
-            
-            Console.Write("Digite o valor C: ");
-            valorC = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.Clear();
 
-            if (valorA > valorB && valorB > valorC)
-                Console.WriteLine("{0} {1} {2}", valorA, valorB, valorC);
+                Console.Write("Digite o valor A: ");
+                valorA = Convert.ToInt32(Console.ReadLine());
 
-            else if (valorB > valorA && valorA > valorC)
-                Console.WriteLine("{0} {1} {2}", valorB, valorA, valorC);
+                Console.Write("Digite o valor B: ");
+                valorB = Convert.ToInt32(Console.ReadLine());
 
-            else if (valorC > valorB && valorB > valorA)
-                Console.WriteLine("{0} {1} {2}", valorC, valorB, valorA);
+                Console.Write("Digite o valor C: ");
+                valorC = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+                if (valorA > valorB && valorB > valorC)
+                    Console.WriteLine("{0} {1} {2}", valorA, valorB, valorC);
+
+                else if (valorA > valorB && valorA > valorC && valorC > valorB)
+                    Console.WriteLine("{0} {1} {2}", valorA, valorC, valorB);
+
+                else if (valorB > valorA && valorA > valorC)
+                    Console.WriteLine("{0} {1} {2}", valorB, valorA, valorC);
+
+                else if (valorB > valorA && valorB > valorC && valorC > valorA)
+                    Console.WriteLine("{0} {1} {2}", valorB, valorC, valorA);
+
+                else if (valorC > valorB && valorB > valorA)
+                    Console.WriteLine("{0} {1} {2}", valorC, valorB, valorA);
+
+                else if (valorC > valorB && valorA > valorB)
+                    Console.WriteLine("{0} {1} {2}", valorC, valorA, valorB);
+
+                Console.ReadKey(); 
+            }
         }
     }
 }
